1c58144448f317c978c028da4fc00c79ea161a6b
Dan6erbond <moravrav@gmail.com>
2021-05-25 12:11:04 +0200

    ✨ Add providers and simple nav

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -1,8 +1,26 @@
+import { ChakraProvider, Flex, Heading, Link, Spacer } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import NextLink from "next/link";
+import { QueryClient, QueryClientProvider } from "react-query";
 import "../styles/globals.css";
 
+const queryClient = new QueryClient();
+
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Flex align="center" py={4} px={6} shadow="base">
+          <Heading size="lg">Cats API</Heading>
+          <Spacer />
+          <NextLink href="/liked">
+            <Link color="blue.500">Liked</Link>
+          </NextLink>
+        </Flex>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  );
 }
 
-export default MyApp;
+export default App;
