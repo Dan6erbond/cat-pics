40b6c5d96f3902a9a212650a79bd6c420b0b1007
Dan6erbond <moravrav@gmail.com>
2021-05-25 10:30:40 +0200

    ✨ Setup React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Flex, Link } from "@chakra-ui/react";
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import {
+  BrowserRouter as Router,
+  Link as RouterLink,
+  Route,
+  Switch,
+} from "react-router-dom";
 import "./App.css";
 
 const queryClient = new QueryClient();
@@ -8,9 +14,21 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <div className="App">
-      <QueryClientProvider client={queryClient}>
-        <ChakraProvider></ChakraProvider>
-      </QueryClientProvider>
+      <Router>
+        <QueryClientProvider client={queryClient}>
+          <ChakraProvider>
+            <Flex align="center" justify="flex-end">
+              <Link as={RouterLink} to="/liked">
+                Liked
+              </Link>
+            </Flex>
+            <Switch>
+              <Route path="/liked"></Route>
+              <Route path="/"></Route>
+            </Switch>
+          </ChakraProvider>
+        </QueryClientProvider>
+      </Router>
     </div>
   );
 }
